ce9ef12e2f2257831a390e47c6bd863b26782768
neeloo patel <nilusingh945164@gmai.com>
2024-10-17 10:32:44 +0530

    hk

--- scripct.js
+++ scripct.js
@@ -1,6 +1,8 @@
 
+
 let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
+
 
 function celToFar(){
     let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
